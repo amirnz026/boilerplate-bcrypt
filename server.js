868531583f54ca62ee10d8700eb12909f6b4f02c
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-24 13:25:25 +0400

    made the hashing process async

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 const bcrypt = require('bcrypt')
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
-    console.log(hash);
+bcrypt.hash('passw0rd!', 13, async (err, hash) => {
+    await console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
-        console.log(res); //true
+    bcrypt.compare('passw0rd!', hash, async (err, res) => {
+       await console.log(res); //true
     });
 });
 

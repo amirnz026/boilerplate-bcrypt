14cdd6036304b09baae41744bef141a40e4424d8
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-24 13:04:24 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+console.log('hello world')
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 

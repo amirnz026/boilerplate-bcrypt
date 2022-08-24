39788f567853e1dfd5fb04c500ce7823681899f6
Amir Hosein Nazari <czhablog@gmail.com>
2022-08-24 13:27:04 +0400

    fixed the bug of hashing passwords

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 const bcrypt = require('bcrypt')
-bcrypt.hash('passw0rd!', 13, async (err, hash) => {
-    await console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, async (err, res) => {
-       await console.log(res); //true
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
     });
 });
 
